a35e6e0af2029fec04a19baf2b75b595efd07191
Hansal13 <hansalpatel13@gmail.com>
2025-08-28 13:56:14 +0530

    lab 2 code of python

---
+++ pl2.py
@@ -0,0 +1,136 @@
+#Example code
+
+#1
+# num1, num2 = 10, 30
+# sum= num1+num2
+# print("The sum of",num1,"and",num2,"is:",sum)
+
+#2
+
+# num1, num2 = 10, 30
+# sub= num1-num2
+# print("The subtraction of",num1,"and",num2,"is:",sub)
+
+#3
+# num1, num2 = 10, 30
+# product= num1*num2
+# print("The product of",num1,"and",num2,"is:",product)
+
+#4
+# num1, num2 = 10, 30
+# div = num1/num2
+# print("The division of",num1,"and",num2,"is:",div)
+
+#5
+
+# num1, num2 = 10, 30
+# rem = num1%num2
+# print("The reminder of",num1,"and",num2,"is:",rem)
+
+#6
+
+# num1, num2 = 10, 3
+# exp = num1**num2
+# print("The exponentiation of",num1,"and",num2,"is:",exp)
+
+#7
+# num1, num2 = 10, 3
+# floordiv = num1//num2
+# print("The Floor Division of",num1,"and",num2,"is:",floordiv)
+
+#8
+# x = 8
+# y = 3
+# mod = x % y
+# print (mod)
+
+#9
+
+# a = -5
+# b = 2
+# res1 = a % b
+# print (res1)
+
+#10
+# m = 5
+# n = -2
+# res2 = m % n
+# print (res2)
+
+#11
+# e = -5
+# f = -2
+# res3 = e % f
+# print (res3)
+
+#12
+
+# print (((5 + 4) / 3) * 2)
+
+#13
+
+# x = 3
+# y = 4
+# z = 6
+# print(x*y//z)
+# print(x*(y//z))
+
+#14
+# x = 2
+# y = 3
+# z = 2
+# print(x**y**z)
+# print((x**y)**z)
+
+
+#Post-lab
+
+# pl1
+
+# length = 20
+# width = 10
+
+# area = length*width
+
+# perimeter = 2*(length+width)
+
+# print("area of the rectengle is",area,"and perimeter of the rectengle is",perimeter)
+
+# pl2
+
+# n=int(input("Enter a number : "))
+
+# if (n%2==0):
+#     print("number is even")
+# else :
+#     print("number is odd")
+
+# pl3
+
+# length = 10
+
+# area = 6*length*length
+# volume = length**3
+
+# print("Area of the cube is",area,"and volume of the cube is",volume)
+
+# pl4
+
+# x=10
+# y=20
+
+# z = (x+y)*(x-y)
+
+# print(z)
+
+# pl5
+
+# x=10
+# y=20
+# z = ((x+y)*(x+y))-2*x*y
+# print(z)
+
+# pl6
+cel = 37
+fr = ((9/5)*cel)+32
+print(fr)
