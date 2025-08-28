ab73180d49b8590e2005e633158be7bd2e9dfecb
Hansal13 <hansalpatel13@gmail.com>
2025-08-28 13:54:31 +0530

    !st python lab code

---
+++ pl1.py
@@ -0,0 +1,172 @@
+#1
+# for i in range(5): 
+#     print("Welcome to Python")
+
+#2
+# print("a\t a^2\t a^3")
+
+# for a in range(1, 5):
+#     print(f"{a}\t {a**2}\t {a**3}")
+
+#3
+# a = ((9.5*4.5)-(2.5*3))/(45.5-3.5)
+# print(a)
+
+#example codes
+
+# ex-1
+# x = 5
+# y = -6
+
+# # Performing arithmetic operations
+# sum_result = x + y
+# difference_result = x - y
+# multiplication_result = x * y
+# division_result = x / y
+# # Printing the results
+# print("Sum:", sum_result)
+# print("Difference:", difference_result)
+# print("Multiplication:", multiplication_result)
+# print("Division:", division_result)
+
+#ex-2
+
+# a = 10
+# b = 20
+# # Comparing the values
+# greater_than = a > b
+# less_than_or_equal = a <= b
+# equal_to = a == b
+# not_equal_to = a != b
+# # Printing the results
+# print("Greater than:", greater_than)
+# print("Less than or equal to:", less_than_or_equal)
+# print("Equal to:", equal_to)
+# print("Not equal to:", not_equal_to)
+
+#ex-3
+
+# x = 3.14
+# y = 2.5
+# # Performing arithmetic operations
+# sum_result = x + y
+# difference_result = x - y
+# multiplication_result = x * y
+# division_result = x / y
+# # Printing the results
+# print("Sum:", sum_result)
+# print("Difference:", difference_result)
+# print("Multiplication:", multiplication_result)
+# print("Division:", division_result)
+
+#ex-4
+
+# a = 1.2
+# b = 2.7
+# # Comparing the values
+# greater_than = a > b
+# less_than_or_equal = a <= b
+# equal_to = a == b
+# not_equal_to = a != b
+# # Printing the results
+# print("Greater than:", greater_than)
+# print("Less than or equal to:", less_than_or_equal)
+# print("Equal to:", equal_to)
+# print("Not equal to:", not_equal_to)
+
+#ex-5
+
+# x = 2 + 3j
+# y = -1 + 2j
+# # Performing arithmetic operations
+# sum_result = x + y
+# difference_result = x - y
+# multiplication_result = x * y
+# division_result = x / y
+# # Printing the results
+# print("Sum:", sum_result)
+# print("Difference:", difference_result)
+# print("Multiplication:", multiplication_result)
+# print("Division:", division_result)
+
+#ex-6
+
+# a = 1 + 2j
+# b = 3 + 4j
+# # Comparing the values
+# equal_to = a == b
+# not_equal_to = a != b
+# # Printing the results
+# print("Equal to:", equal_to)
+# print("Not equal to:", not_equal_to)
+
+#ex-7
+
+# st1 = "ICT Department 3EK1"
+# print(st1)
+# print(st1[0])
+# print(st1[0:4])
+
+
+#ex-8
+
+# st1 = "ICT"
+# st2 = "Department"
+# st3 = "3EK1"
+# print(st1+st2+st3)
+# print(4*st1) 
+
+# st1 = "ICT Department 3EK1"
+# print("p" in st1)
+# print("f" not in st1)
+
+#Collection Data Types
+
+# #1
+# list1=[123,567,89] 
+# print(list1)
+
+# list2=["hello","how","are"] 
+# print(list2)
+
+# list3= ["hey",1223,"hello"] 
+# print(list3)
+
+# #2
+
+# list1=["apple","mango",123,345]
+# list2 = ["grapes"]
+# print(list1+ list2)
+
+#3
+
+# dict1={"comp": "computer" , "sci" : "science"}
+# print(dict["comp"])
+# dict2={"123":"computer",456 : "maths"}
+# print(dict2["123"])
+# print(dict1["comp"]+ dict2["123"])
+
+# #Check
+# print(dict1+dict2)
+# print(dict1["computer"]+ dict2["computer"])
+
+
+#4
+
+# my_set = {1, 2, 3, 4, 5}
+# print(my_set)
+
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {4, 5, 6, 7, 8}
+# # check 
+# print(set1 + set2)
+
+#5
+
+my_tuple = (1, 2, 3, 4, 5)
+t1 = (2,3,4)
+t2 = (5,6,7)
+print(t1+t2)
+
+
+
