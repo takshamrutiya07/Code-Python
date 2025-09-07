90485af078ca64ff34425d3a212c4e0c5089cb59
takshamrutiya07 <takshamrutiya@gmail.com>
2025-09-07 13:11:23 +0530

    Lab Programs Python

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-from numpy import linalg as LA
-a = np.array([[1, 0], [1, 2]])
-# Display the original 2x2 array 'a'
-print("Original 2-d array")
-print(a)
-print("Determinant of the said 2-D array:")
-print(np.linalg.det(a))
