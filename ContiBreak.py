e366a08914524ad2c1e15eafc9daf1ba56397d64
Taxxh <takshamrutiya@gmail.com>
2026-05-06 23:20:51 +0530

    Add files via upload

---
+++ ContiBreak.py
@@ -0,0 +1,11 @@
+for i in range (5):#break statement
+    if(i==3):
+        break
+    print(i)
+    
+print("_________________________________________ ")
+#continue statement
+for i in range(5):
+    if(i==3):
+        continue
+    print(i)
