401b3f9d7bfc2df08164af14f90cc9235ef748bc
Taxxh <takshamrutiya@gmail.com>
2026-05-08 22:02:27 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,11 @@
+def isGreater(a,b):
+    if(a>b):
+        print("First number is greater!")
+    else:
+        print("Second number is greater!")
+
+def isLesser(a,b):
+    pass #if we don't want to write the logic of function for now then we need to write pass
+a=7
+b=6
+isGreater(a,b)    
