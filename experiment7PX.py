358890e7b5db435d30faaaf6732785d2816ac887
Taxxh <takshamrutiya@gmail.com>
2026-06-07 22:40:06 +0530

    Add files via upload

---
+++ experiment7PX.py
@@ -0,0 +1,36 @@
+#makin 3x3 matrix in range of 2 to 10
+
+import numpy as np
+# matrix = np.arange(2,11).reshape(3,3)
+# print(matrix)
+
+
+# # reverse an arry
+# a= np.array([1,2,3,4,5,6,7,8])
+# print(a[::-1])
+
+#find common values between 2 arrays
+# a= np.array([1,2,3,4,5,6,7,8])
+# b = np.array([5,6,2,7,8,9,])
+# for i in a :
+#     if(i in b):
+#         print(i)
+# repeat array element
+
+# b=[[1,5,9],[1,2,3]]
+# a=np.array(b)
+# r= np.repeat(a,2)
+# print(r)
+
+# # memory size of array
+
+# arr = np.array([10, 20, 30, 40, 50], dtype=np.int32)
+# size = arr.itemsize
+# print(size)
+
+
+
+# print(np.zeros(5))
+# print(np.zeros([2,5]))
+# print(np.ones(5))
+print(np.ones([2,5]))
